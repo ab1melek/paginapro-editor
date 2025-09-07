d7a0fd55960ed73293addcaa814c451553cd1b26
Abimelek Castrezana <abimelekcastrezana@gmail.com>
2025-09-07 00:44:16 -0600

    feat: se le agregó el alineamiento al blocktune

--- components/utils/tools.js
+++ components/utils/tools.js
@@ -10,24 +10,25 @@ import Marker from '@editorjs/marker'
 import Paragraph from '@editorjs/paragraph'
 import Quote from '@editorjs/quote'
 import Raw from '@editorjs/raw'
-import SimpleImage from '@editorjs/simple-image'
 import Table from '@editorjs/table'
 import Warning from '@editorjs/warning'
 import ColorPicker from 'editorjs-color-picker'
+import AlignmentTuneTool from "editorjs-text-alignment-blocktune"
 import FontEditorTool from '../editorPlugins/FontEditorTool'
-
 
 export const EDITOR_JS_TOOLS = {
   paragraph: {
     class: Paragraph,
     inlineToolbar: true,
+    tunes: ["alignment"],
   },
   header: {
     class: Header,
     inlineToolbar: true,
+    tunes: ["alignment"],
     config: {
       levels: [2, 3, 4],
-      defaultLevel: 2,
+      defaultLevel: 2, 
     },
   },
   list: {
@@ -65,7 +66,16 @@ export const EDITOR_JS_TOOLS = {
       },
     },
   },
-  simpleImage: SimpleImage,
+  alignment: {
+    class: AlignmentTuneTool,
+    config: {
+      default: "left",
+      blocks: {
+        header: "center",
+        paragraph: "left",
+      },
+    },
+  },
   raw: Raw,
   delimiter: Delimiter,
   marker: Marker,
